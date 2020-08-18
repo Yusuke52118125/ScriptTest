2c6ee0d9abc65e454e1c0034c42e4ac7be88788d
yusuke.teramoto <bb52118125@icloud.com>
2020-08-18 22:32:46 +0900

    lesson4の課題

---
+++ Assets/Test.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Boss
+{
+    private int hp = 100;          // 体力
+    private int power = 25; // 攻撃力
+    private int mp = 53; //mp
+
+    // 攻撃用の関数
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    // 防御用の関数
+    public void Defence(int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+        // 残りhpを減らす
+        this.hp -= damage;
+    }
+    public void Magic(int magic)
+    {
+        for( int i = 100 ;0 <= i ; i--)
+        {
+            if(5 <= mp)
+            {
+                mp -= magic;
+                Debug.Log("魔法攻撃した。残りMPは" + mp + "。");
+            }
+            else
+            {
+                Debug.Log("mpが足りないための方が使えない");
+                break;
+
+            }
+        }
+    }
+
+}
+
+public class Test : MonoBehaviour
+{
+
+    void Start()
+    {
+        // Bossクラスの変数を宣言してインスタンスを代入
+        Boss lastboss = new Boss();
+
+        // 攻撃用の関数を呼び出す
+        lastboss.Attack();
+        // 防御用の関数を呼び出す
+        lastboss.Defence(3);
+        lastboss.Magic(5);
+
+        int[] array = { 10, 20, 30, 40, 50 };
+        for (int i = 0; i < array.Length; i++)
+        {
+            Debug.Log(array[i]);
+        }
+        for(int j = 4; 0 <= j; j--)
+        {
+            Debug.Log(array[j]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
